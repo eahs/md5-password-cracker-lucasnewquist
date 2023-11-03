41bcc9c8c83b0ec3c8385ea310ded0b458dce24b
Lucas Newquist <lightblueviolin@gmail.com>
2023-11-03 17:52:14 -0400

    Completed version
    takes a really long time but it works!

--- PasswordCracker/PasswordCracker/Program.cs
+++ PasswordCracker/PasswordCracker/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace PasswordCracker
@@ -45,10 +46,92 @@ namespace PasswordCracker
             {
                 Console.WriteLine(pass);
             }
+            Dictionary<int, String> letters = new Dictionary<int, String>();
+            Dictionary<int, List<String>> password = new Dictionary<int, List<String>>();
 
-            // Use this method to test if you managed to correctly crack all the passwords
-            // Note that hashedPasswords will need to be swapped out with an array the exact
-            // same length that contains all the cracked passwords
+            letters.Add(1, "a");
+            letters.Add(2, "b");
+            letters.Add(3, "c");
+            letters.Add(4, "d");
+            letters.Add(5, "e");
+            letters.Add(6, "f");
+            letters.Add(7, "g");
+            letters.Add(8, "h");
+            letters.Add(9, "i");
+            letters.Add(10, "j");
+            letters.Add(11, "k");
+            letters.Add(12, "l");
+            letters.Add(13, "m");
+            letters.Add(14, "n");
+            letters.Add(15, "o");
+            letters.Add(16, "p");
+            letters.Add(17, "q");
+            letters.Add(18, "r");
+            letters.Add(19, "s");
+            letters.Add(20, "t");
+            letters.Add(21, "u");
+            letters.Add(22, "v");
+            letters.Add(23, "w");
+            letters.Add(24, "x");
+            letters.Add(25, "y");
+            letters.Add(26, "z");
+
+            String begin;
+            int counter = 0;
+
+            List<String> nums = new List<String>();
+            List<String> unhashed = new List<String>();
+
+            for (int i = 1; i <= 26; i++)
+            {
+                for (int j = 1; j <= 26; j++)
+                {
+
+                    for (int k = 1; k <= 26; k++)
+                    {
+
+                        for (int l = 1; l <= 26; l++)
+                        {
+
+                            for (int p = 1; p <= 26; p++)
+                            {
+
+                                begin = letters[i] + letters[j] + letters[k] + letters[l] + letters[p];
+
+                                nums.Add(md5(begin));
+                                unhashed.Add(begin);
+
+                                counter++;
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                
+                Console.WriteLine(nums);
+            }
+
+            Console.Write(counter);
+
+            for (int i = 0; i < hashedPasswords.Length; i++)
+            {
+                for (int j = 0; j < nums.Count; j++)
+                {
+                    if (hashedPasswords[i] == nums[j])
+                    {
+                        Console.WriteLine("hashed: " + hashedPasswords[i] + "generated: " + nums[j]);
+                        hashedPasswords[i] = unhashed[j];
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+            }
+           
+
+
             bool passwordsValidated = Validator.ValidateResults(hashedPasswords);
             
             Console.WriteLine($"\nPasswords successfully cracked: {passwordsValidated}");
